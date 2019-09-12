68d42b9a3fc57dc9a955120b6581f5c1d6689728
Daniela <dennysdanielamichelorihuela@gmail.com>
2019-09-12 14:19:58 -0400

    mi codigo

---
+++ src/main/java/com/consultorio/consultorio/consultorio.java
@@ -0,0 +1,21 @@
+package com.consultorio.consultorio;
+import lombok.Data;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Data
+@Entity
+
+class consultorio {
+                        private @Id @GeneratedValue Long id;
+                        private String numero;
+                        private String piso;
+                        private String especialidad;
+                        Employee() {}
+                        Employee(String numero, String piso, String especialidad)
+                      { this.numero = numero;
+                      this.piso = piso;
+                      this.especialidad = especialidad;}
+}
+
